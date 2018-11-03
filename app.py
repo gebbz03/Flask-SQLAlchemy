1376be175e385e8bb1e7764c75e42354a3cc15e9
Gebb <gebb.freelancer@gmail.com>
2018-11-03 19:50:45 +0800

    last-commit

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.secret_key='gebbz'
 api=Api(app)
 
-
+'''
 @app.before_first_request
 def create_tables():
     db.create_all()
+'''    
 
 jwt=JWT(app,authenticate,identity) # /auth
 
